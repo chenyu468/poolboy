5f7e8739120920908af66a8a5a2d7b02a64eda3d
chenyu <1916310286@qq.com>
2018-04-17 13:01:00 +0800

    no message

--- src/poolboy_sup_2.erl
+++ src/poolboy_sup_2.erl
@@ -1,6 +1,6 @@
 %% Poolboy - A hunky Erlang worker pool factory
 
--module(poolboy_sup).
+-module(poolboy_sup_2).
 -behaviour(supervisor).
 
 -export([start_link/2, init/1]).
@@ -10,6 +10,6 @@ start_link(Mod, Args) ->
 
 init({Mod, Args_b}) ->
     Args = Args_b ++ [{parent_pid,self()}],
-    {ok, {{simple_one_for_one, 0, 1},
+    {ok, {{one_for_one, 0, 1},
           [{Mod, {Mod, start_link, [Args]},
             temporary, 5000, worker, [Mod]}]}}.
